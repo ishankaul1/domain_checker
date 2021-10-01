0a84c1c98e781fa0451d94df074f4334f02e3f1a
Ishan Kaul <ishankaul2000@gmail.com>
2021-10-01 00:02:46 -0700

    domain checker implemented; need to write some unit tests

---
+++ domain_checker.py
@@ -0,0 +1,25 @@
+#Instructions:
+#   Write a program that maintains a list of “trusted” domains and takes as input an arbitrarily-sized list of other domains and a number. 
+#   The function will check each input domain and if the Levenshtein distance is below the input number, list the domain as risky along with the domain that it is similar to and the distance.
+
+
+from edit_distance import editDist_DP
+
+#Function that compares list of trusted domains to a list of received domains, and returns a list of 'bad' domains.
+#Each bad domain will be returned in the format - [Received domain, similar domain, edit distance]
+#Note if a received domain matches several trustworthy domains, the list will include all domains it matched with
+def domain_check(trusted, received, threshold):
+    risky = []
+    for rec_domain in received:
+        for trust_domain in trusted:
+            dist = editDist_DP(rec_domain, trust_domain)
+            if dist < threshold:
+                risky.append([rec_domain, trust_domain, dist])
+
+    return risky
+
+def main():
+    print(domain_check(['twltter.com', 'poopyscoop.com', 'yopmail.com'], ['twitter.com', 'facebook.com', 'google.com'], 3))
+
+
+main()
